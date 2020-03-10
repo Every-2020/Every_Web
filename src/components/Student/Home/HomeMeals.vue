56012a999ba9f8c2281ae35579aa1c9f16f4bad6
Sh031224 <1cktmdgh2@gmail.com>
2020-03-10 10:48:55 +0900

    [ADD] Main 반응형

--- src/components/Student/Home/HomeMeals.vue
+++ src/components/Student/Home/HomeMeals.vue
@@ -3,12 +3,14 @@
     <span class="home_right_title">한국인은 밥심.</span>
     <span class="home_right_h1">오늘의 급식을 한눈에.</span>
     <div class="home_right_meals">
+      <img @click="prev" class="home_right_meals_left" src="../../../assets/student/home/meal_next.png" alt="prev">
       <div class="home_right_meals_1 home_right_meals_box">
         <div class="home_right_meals_box_title">
           <img src="../../../assets/student/home/sunrise.png" alt="sunrise">
           <span>아침</span>
         </div>
         <div class="home_right_meals_box_content" v-if="meals[0]" v-html="meals[0].meal_name"></div>
+        <div class="home_right_meals_box_content" v-else>급식이 없습니다.</div>
         <img class="home_right_meals_wave" src="../../../assets/student/home/meals-wave.png" alt="wave">
       </div>
       <div class="home_right_meals_2 home_right_meals_box">
@@ -17,6 +19,7 @@
           <span>점심</span>
         </div>
         <div class="home_right_meals_box_content" v-if="meals[1]" v-html="meals[1].meal_name"></div>
+        <div class="home_right_meals_box_content" v-else>급식이 없습니다.</div>
         <img class="home_right_meals_wave" src="../../../assets/student/home/meals-wave2.png" alt="wave">
       </div>
       <div class="home_right_meals_3 home_right_meals_box">
@@ -25,8 +28,10 @@
           <span>저녁</span>
         </div>
         <div class="home_right_meals_box_content" v-if="meals[2]" v-html="meals[2].meal_name"></div>
+        <div class="home_right_meals_box_content" v-else>급식이 없습니다.</div>
         <img class="home_right_meals_wave" src="../../../assets/student/home/meals-wave2.png" alt="wave">
       </div>
+      <img @click="next" class="home_right_meals_right" src="../../../assets/student/home/meal_next.png" alt="next">
     </div>
   </div>
 </template>
@@ -40,7 +45,8 @@ export default {
     return {
       url: server,
       meals: [],
-      noMeals: true
+      noMeals: true,
+      now: 2
     }
   },
   mounted() {
@@ -55,11 +61,44 @@ export default {
       this.noMeals = true
     })
   },
+  methods: {
+    prev() {
+      if (this.now > 1) {
+        if (this.now === 3) {
+          document.getElementsByClassName('home_right_meals_right')[0].classList.remove('ob')
+        }
+        document.getElementsByClassName(`home_right_meals_${this.now}`)[0].style.display = "none"
+        this.now -= 1
+        document.getElementsByClassName(`home_right_meals_${this.now}`)[0].style.display = "block"    
+        if (this.now === 1) {
+          document.getElementsByClassName('home_right_meals_left')[0].classList.add('ob')
+        }
+      }
+    },
+    next() {
+      if (this.now < 3) {
+        if (this.now === 1) {
+          document.getElementsByClassName('home_right_meals_left')[0].classList.remove('ob')
+        }
+        document.getElementsByClassName(`home_right_meals_${this.now}`)[0].style.display = "none"
+        this.now += 1
+        document.getElementsByClassName(`home_right_meals_${this.now}`)[0].style.display = "block"    
+        if (this.now === 3) {
+          document.getElementsByClassName('home_right_meals_right')[0].classList.add('ob')
+        }
+      }
+    }
+  },
 }
 </script>
 
 <style lang="scss">
+.ob {
+  opacity: 0.3;
+  filter: alpha(opacity=30);
+}
 .home_right {
+  z-index: 10;
   margin-top: 100px;
   margin-left: 40px;
   border-radius: 16px;
@@ -74,13 +113,39 @@ export default {
   flex-direction: column;
   -ms-flex-direction: column;
   text-align: left;
+  @media screen and (max-width: 768px) {
+    max-width: 100%;
+    width: 100%;
+    margin-left: 0;
+    margin-top: 20px;
+    overflow: hidden;
+    padding: 30px;
+  }
+  @media screen and (max-width: 450px) {
+    padding: 20px;
+  }
+  @media screen and (max-width: 350px) {
+    margin-top: 10px;
+  }
   &_h1 {
     font-size: 31px;
     font-weight: 800;
+    @media screen and (max-width: 768px) {
+      font-size: 27px;
+    }
+    @media screen and (max-width: 450px) {
+      font-size: 18px;
+    }
   }
   &_title {
     font-size: 29px;
     font-weight: 800;
+    @media screen and (max-width: 768px) {
+      font-size: 25px;
+    }
+    @media screen and (max-width: 450px) {
+      font-size: 20px;
+    }
   }
   &_meals {
     flex-grow: 1;
@@ -88,6 +153,34 @@ export default {
     display: flex;
     display: -webkit-flex;
     justify-content: space-between;
+    @media screen and (max-width: 768px) {
+      justify-content: center;
+      align-items: center;
+      margin-top: 0px;
+    }
+    &_left {
+      width: 20px;
+      height: 20px;
+      transform: rotate(180deg);
+      -o-transform: rotate(180deg);
+      -ms-transform: rotate(180deg);
+      -moz-transform: rotate(180deg);
+      -webkit-transform: rotate(180deg);
+      cursor: pointer;
+      margin-right: 10px;
+      @media screen and (min-width: 768px) {
+        display: none;
+      }
+    }
+    &_right {
+      cursor: pointer;
+      height: 20px;
+      width: 20px;
+      margin-left: 10px;
+      @media screen and (min-width: 768px) {
+        display: none;
+      }
+    }
     &_box {
       box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.116);
       width: 32%;
@@ -96,10 +189,20 @@ export default {
       height: 210px;
       position: relative;
       padding: 13px;
+      @media screen and (max-width: 768px) {
+        width: 70%;
+        max-width: 250px;
+      }
       &_title {
         color: white;
         font-weight: 800;
         font-size: 18px;
+        display: flex;
+        display: -webkit-flex;
+        align-items: center;
+        span {
+          margin-left: 5px;
+        }
         img {
           width: 22px;
         }
@@ -108,7 +211,7 @@ export default {
         color: white;
         font-size: 14px;
         text-align: center;
-        font-weight: 400;
+        font-weight: 600;
         line-height: 25px;
         height: 140px;
         overflow-y: scroll;
@@ -121,12 +224,24 @@ export default {
     }
     &_1 {
       background-color: #611CEE;
+      display: none;
+      @media screen and (min-width: 768px) {
+        display: block !important;
+      }
     }
     &_2 {
       background-color: #4904D8;
+      display: block;
+      @media screen and (min-width: 768px) {
+        display: block !important;
+      }
     }
     &_3 {
       background-color: #3A00B4;
+      display: none;
+      @media screen and (min-width: 768px) {
+        display: block !important;
+      }
     }
     &_wave {
       position: absolute;
---
+++ src/views/Student/Schedule/Schedule.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="schedule">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'schedule',
+  mounted () {
+    document.getElementsByClassName('router_home')[0].classList.remove('active_home')
+    document.getElementsByClassName('router_bamboo')[0].classList.remove('active_bamboo')
+    document.getElementsByClassName('router_schedule')[0].classList.add('active_schedule')
+  },
+}
+</script>
+
+<style>
+
+</style>
