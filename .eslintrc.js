e7a12f35cc225d0cca9015b2cb91f81faa6b85a7
Sh031224 <1cktmdgh2@gmail.com>
2020-03-24 13:27:06 +0900

    [ADD] eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
   // add your custom rules here
   rules: {
     'no-console': 'off',
-    'vue/no-v-html': 'off'
+    'vue/no-v-html': 'off',
+    'no-useless-escape': 'off'
   }
 }
